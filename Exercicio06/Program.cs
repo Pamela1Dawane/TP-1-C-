fb4baa2eafbea1e30017ccc7f1fcd1282b1b1926
Pamela1Dawane <pamela.dawane1@gmail.com>
2026-08-10 22:28:25 -0300

    Organiza estrutura dos exercícios

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b, temp;
+
+        a = 1;
+        b = 2;
+        Console.WriteLine($"a = {a}, b = {b}");
+        temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine($"a = {a}, b = {b}");
+    }
+}
